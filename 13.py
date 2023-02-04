7e38344b6fd6551ae5f8fee5057863e38a984b65
Arash <53490252+ThePythonist@users.noreply.github.com>
2023-02-04 09:50:02 +0330

    Add files via upload

---
+++ 13.py
@@ -0,0 +1,9 @@
+n1 = int(input("Enter number1 : "))
+n2 = int(input("Enter number2 : "))
+
+if n1 > n2:
+    print("Number1 is bigger")
+elif n1 == n2:
+    print("Numbers are equal")
+else:
+    print("Number2 is bigger")
---
+++ 17.py
@@ -0,0 +1,9 @@
+number = int(input("Enter a number : "))
+
+if number == 0:
+    print("The number is zero")
+else:
+    if number % 2 == 0:
+        print("The number is even")
+    else:
+        print("The number is odd ")
