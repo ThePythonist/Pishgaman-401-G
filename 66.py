6801a6a536ca2052db20be100bbd1a202aa08cf0
Arash <53490252+ThePythonist@users.noreply.github.com>
2023-03-04 20:17:32 +0330

    Add files via upload

---
+++ 66.py
@@ -0,0 +1,19 @@
+# def greetings(name):
+#     print("Hello", name)
+#
+#
+# names = ["ahmad", "arshia", "ali", "diba", "arad"]
+# for i in names:
+#     greetings(i)
+
+# ---------------------------------------
+
+def greetings(name):
+    print("Run")
+    return "Hello" + " " + name
+
+
+# names = ["ahmad", "arshia", "ali", "diba", "arad"]
+# for i in names:
+#     greetings(i)
+print(greetings("Atefe"))
---
+++ 67.py
@@ -0,0 +1,30 @@
+def check_number(num):
+    if num % 2 == 0:
+        print("The number is even")
+    else:
+        print("The number is odd")
+
+
+# while True:
+#     check_number(int(input("Enter any number : ")))
+
+numbers = [12, 13, 14, 15, 16]
+
+for i in numbers:
+    check_number(i)
+
+# ---------------------------------------
+
+# def check_number(num):
+#     if num % 2 == 0:
+#         return "Even"
+#     else:
+#         return "Odd"
+#
+#
+# print(check_number(20))
+#
+# if check_number(20) == "Even":
+#     print("Yes")
+# else:
+#     print("No")
